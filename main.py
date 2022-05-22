791322ffa6dc28e9c14e24514e6c9d516aba6032
Carrie <creed3@dacc.edu>
2022-05-21 19:02:30 -0700

    Loaded images in

--- main.py
+++ main.py
@@ -3,5 +3,19 @@ import os
 import time
 import random
 
-# Load the images
+# Load the Enemy ships
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
+
+# Load the Player ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_yellow.png"))
+
+# Load the Lasers
+RED_LASER = pygame.image.load(os.path.join("assets","pixel_laser_red.png"))
+GREED_LASER = pygame.image.load(os.path.join("assets","pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets","pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets","pixel_laser_yellow.png"))
+
+# Load the background image
+BG = pygame.image.load(os.path.join("assets","background-black.png"))
